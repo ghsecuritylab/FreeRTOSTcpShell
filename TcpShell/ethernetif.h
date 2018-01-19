725a681bb3af28f91ab81b78be36ce8b6425cf42
dan parker <silverliingsw@hotmail.com>
2018-01-18 20:36:14 -0800

    Fixed now?

--- TcpShell/ethernetif.h
+++ TcpShell/ethernetif.h
@@ -6,6 +6,9 @@
 #include "lwip/netif.h"
 #include "cmsis_os.h"
 
+// VisualGDB doesn't #define this. Are we using a different board rev from them?
+#define LAN8742A_PHY_ADDRESS            0x00U
+
 /* Exported types ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 err_t ethernetif_init(struct netif *netif);
